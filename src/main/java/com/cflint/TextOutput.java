952c5874ec89fb4aea7cb6077658b8c285cb3ead
ryaneberly <ryaneberly@gmail.com>
2014-04-11 23:16:12 -0400

    Changed output to match suggestions
    https://github.com/ryaneberly/CFLint/pull/5

--- src/main/java/com/cflint/TextOutput.java
+++ src/main/java/com/cflint/TextOutput.java
@@ -11,16 +11,16 @@ public class TextOutput {
 		int counter = 0;
 		for (final Entry<String, List<BugInfo>> bugEntry : bugList.getBugList().entrySet()) {
 			sb.append("Issue");
-			sb.append("\r\n severity:").append(bugEntry.getValue().get(0).getSeverity());
-			sb.append("\r\n id:").append(bugEntry.getValue().get(0).getMessageCode());
-			sb.append("\r\n message code:").append(bugEntry.getValue().get(0).getMessageCode());
+			sb.append("\r\nSeverity:").append(bugEntry.getValue().get(0).getSeverity());
+//			sb.append("\r\nid:").append(bugEntry.getValue().get(0).getMessageCode());
+			sb.append("\r\nMessage code:").append(bugEntry.getValue().get(0).getMessageCode());
 			for (final BugInfo bugInfo : bugEntry.getValue()) {
 				counter++;
-				sb.append("\r\n\tfile:").append(bugInfo.getFilename());
-				sb.append(Integer.valueOf(bugInfo.getColumn()).toString());
-				sb.append(" [").append(Integer.valueOf(bugInfo.getLine()).toString()).append("]");
-				sb.append("\r\n\t\tmessage:").append(bugInfo.getMessage());
-				sb.append("\r\n\t\tvariable:'").append(bugInfo.getVariable());
+				sb.append("\r\n\tFile:").append(bugInfo.getFilename());
+				sb.append("\r\n\tColumn:").append(Integer.valueOf(bugInfo.getColumn()).toString());
+				sb.append("\r\n\tLine:").append(Integer.valueOf(bugInfo.getLine()).toString());
+				sb.append("\r\n\t\tMessage:").append(bugInfo.getMessage());
+				sb.append("\r\n\t\tVariable:'").append(bugInfo.getVariable());
 				sb.append("' in function: ").append(bugInfo.getFunction());
 				sb.append("\r\n\t\tExpression:").append(bugInfo.getExpression());
 				sb.append("\r\n");

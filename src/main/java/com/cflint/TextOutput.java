5ad196671d71744ca0c6ad198142977c571dc7f3
ryaneberly <ryaneberly@gmail.com>
2014-08-11 21:52:56 -0400

    Merged Machine independent newline #14

--- src/main/java/com/cflint/TextOutput.java
+++ src/main/java/com/cflint/TextOutput.java
@@ -7,25 +7,26 @@ import java.util.Map.Entry;
 
 public class TextOutput {
 
+	final static String newLine = System.getProperty("line.separator");
 	public void output(final BugList bugList, final Writer sb) throws IOException {
 		int counter = 0;
 		for (final Entry<String, List<BugInfo>> bugEntry : bugList.getBugList().entrySet()) {
 			sb.append("Issue");
 			for (final BugInfo bugInfo : bugEntry.getValue()) {
-				sb.append("\r\nSeverity:").append(bugEntry.getValue().get(0).getSeverity());
-				sb.append("\r\nMessage code:").append(bugEntry.getValue().get(0).getMessageCode());
+				sb.append(newLine).append("Severity:").append(bugEntry.getValue().get(0).getSeverity());
+				sb.append(newLine).append("Message code:").append(bugEntry.getValue().get(0).getMessageCode());
 				counter++;
-				sb.append("\r\n\tFile:").append(bugInfo.getFilename());
-				sb.append("\r\n\tColumn:").append(Integer.valueOf(bugInfo.getColumn()).toString());
-				sb.append("\r\n\tLine:").append(Integer.valueOf(bugInfo.getLine()).toString());
-				sb.append("\r\n\t\tMessage:").append(bugInfo.getMessage());
-				sb.append("\r\n\t\tVariable:'").append(bugInfo.getVariable());
+				sb.append(newLine).append("\tFile:").append(bugInfo.getFilename());
+				sb.append(newLine).append("\tColumn:").append(Integer.valueOf(bugInfo.getColumn()).toString());
+				sb.append(newLine).append("\tLine:").append(Integer.valueOf(bugInfo.getLine()).toString());
+				sb.append(newLine).append("\t\tMessage:").append(bugInfo.getMessage());
+				sb.append(newLine).append("\t\tVariable:'").append(bugInfo.getVariable());
 				sb.append("' in function: ").append(bugInfo.getFunction());
-				sb.append("\r\n\t\tExpression:").append(bugInfo.getExpression());
-				sb.append("\r\n");
+				sb.append(newLine).append("\t\tExpression:").append(bugInfo.getExpression());
+				sb.append(newLine);
 			}
 		}
-		sb.append("\r\nTotal issues:" + counter);
+		sb.append(newLine).append("Total issues:" + counter);
 		sb.flush();
 		sb.close();
 	}

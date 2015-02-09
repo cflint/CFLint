398909a3e084b1d473f253bc3d0360c94e138dd8
ryaneberly <ryaneberly@gmail.com>
2015-02-08 20:46:47 -0500

    CFXTagChecker accepts a regex string

--- src/main/java/com/cflint/plugins/core/CFXTagChecker.java
+++ src/main/java/com/cflint/plugins/core/CFXTagChecker.java
@@ -12,7 +12,7 @@ public class CFXTagChecker extends CFLintScannerAdapter {
 	public void element(final Element element, final Context context, final BugList bugs) {
 		final String tagName = element.getName();
 		final String cfmlTagCheck = getParameter("tagName");
-		if (tagName.contains(cfmlTagCheck)) {
+		if (tagName.contains(cfmlTagCheck) || tagName.matches(cfmlTagCheck)) {
 			// final int begLine =
 			// element.getSource().getRow(element.getBegin());
 			context.addMessage("AVOID_USING_" + tagName.toUpperCase() + "_TAG", tagName);
---
+++ src/test/java/com/cflint/TestCFCustomTagChecker.java
@@ -0,0 +1,45 @@
+package com.cflint;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cfml.parsing.cfscript.ParseException;
+
+import com.cflint.config.CFLintPluginInfo.PluginInfoRule;
+import com.cflint.config.CFLintPluginInfo.PluginInfoRule.PluginMessage;
+import com.cflint.config.ConfigRuntime;
+import com.cflint.plugins.core.CFXTagChecker;
+
+public class TestCFCustomTagChecker {
+
+	private CFLint cfBugs;
+
+	@Before
+	public void setUp() {
+		final ConfigRuntime conf = new ConfigRuntime();
+		final PluginInfoRule pluginRuleX = new PluginInfoRule();
+		pluginRuleX.setName("CFXTagChecker");
+		pluginRuleX.addParameter("tagName", ".*:cfcustomtag");
+		conf.getRules().add(pluginRuleX);
+		final PluginMessage pluginMessageX = new PluginMessage("AVOID_USING_CFUPDATE_TAG");
+		pluginMessageX.setSeverity("WARNING");
+		pluginMessageX
+				.setMessageText("Avoid Leaving <${tagName}> tags in committed code. Debug information should be ommited from release code");
+		pluginRuleX.getMessages().add(pluginMessageX);
+		final CFXTagChecker checker = new CFXTagChecker();
+		checker.setParameter("tagName", ".*:cfcustomtag");
+		cfBugs = new CFLint(conf, checker);
+	}
+
+	@Test
+	public void test_BAD() throws ParseException, IOException {
+		final String cfcSrc = "<mylib:cfcustomtag x=1/>";
+		cfBugs.process(cfcSrc, "test");
+		assertEquals(1, cfBugs.getBugs().getBugList().size());
+	}
+
+}

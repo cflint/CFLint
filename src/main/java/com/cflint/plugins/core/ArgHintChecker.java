487df117df6680c2fdc12fd7a8e24cd0a50f8e65
Justin Mclean <justin@classsoftware.com>
2015-09-19 13:39:29 +1000

    Changed to use BugInfoBuilder

--- src/main/java/com/cflint/plugins/core/ArgHintChecker.java
+++ src/main/java/com/cflint/plugins/core/ArgHintChecker.java
@@ -2,20 +2,25 @@ package com.cflint.plugins.core;
 
 import net.htmlparser.jericho.Element;
 
+import com.cflint.BugInfo;
 import com.cflint.BugList;
 import com.cflint.plugins.CFLintScannerAdapter;
 import com.cflint.plugins.Context;
 
 public class ArgHintChecker extends CFLintScannerAdapter {
+	final String severity = "INFO";
+
 	@Override
 	public void element(final Element element, final Context context, final BugList bugs) {
 		if (element.getName().equals("cfargument")) {
 			final String name = element.getAttributeValue("name");
 			final String hint = element.getAttributeValue("hint");
 			if (hint == null || hint.length() == 0) {
-				element.getSource().getRow(element.getBegin());
-				element.getSource().getColumn(element.getBegin());
-				context.addMessage("ARG_HINT_MISSING", name);
+				int begLine = element.getSource().getRow(element.getBegin());
+				bugs.add(new BugInfo.BugInfoBuilder().setLine(begLine).setMessageCode("ARG_HINT_MISSING")
+					.setSeverity(severity).setFilename(context.getFilename())
+					.setMessage("Argument " + name + " is missing a hint.")
+					.build());
 			}
 		}
 	}
--- src/main/java/com/cflint/plugins/core/FunctionHintChecker.java
+++ src/main/java/com/cflint/plugins/core/FunctionHintChecker.java
@@ -2,20 +2,25 @@ package com.cflint.plugins.core;
 
 import net.htmlparser.jericho.Element;
 
+import com.cflint.BugInfo;
 import com.cflint.BugList;
 import com.cflint.plugins.CFLintScannerAdapter;
 import com.cflint.plugins.Context;
 
 public class FunctionHintChecker extends CFLintScannerAdapter {
+	final String severity = "INFO";
+
 	@Override
 	public void element(final Element element, final Context context, final BugList bugs) {
 		if (element.getName().equals("cffunction")) {
 			final String name = element.getAttributeValue("name");
 			final String hint = element.getAttributeValue("hint");
 			if (hint == null || hint.length() == 0) {
-				element.getSource().getRow(element.getBegin());
-				element.getSource().getColumn(element.getBegin());
-				context.addMessage("FUNCTION_HINT_MISSING", name);
+				int begLine = element.getSource().getRow(element.getBegin());
+				bugs.add(new BugInfo.BugInfoBuilder().setLine(begLine).setMessageCode("FUNCTION_HINT_MISSING")
+					.setSeverity(severity).setFilename(context.getFilename())
+					.setMessage("Function " + name + " is missing a hint.")
+					.build());
 			}
 		}
 	}

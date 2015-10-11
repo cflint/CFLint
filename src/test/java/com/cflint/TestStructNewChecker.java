3a4b229a11fcd1fa5e1a4e3749b7d21b916dbfb8
Justin Mclean <justin@classsoftware.com>
2015-10-12 00:02:28 +1100

    tests for structNew rule

---
+++ src/test/java/com/cflint/TestStructNewChecker.java
@@ -0,0 +1,59 @@
+package com.cflint;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cfml.parsing.reporting.ParseException;
+
+import com.cflint.config.CFLintPluginInfo.PluginInfoRule;
+import com.cflint.config.CFLintPluginInfo.PluginInfoRule.PluginMessage;
+import com.cflint.config.ConfigRuntime;
+import com.cflint.plugins.core.StructNewChecker;
+
+public class TestStructNewChecker {
+
+	private CFLint cfBugs;
+
+	@Before
+	public void setUp() {
+		final ConfigRuntime conf = new ConfigRuntime();
+		final PluginInfoRule pluginRule = new PluginInfoRule();
+		pluginRule.setName("StructNewChecker");
+		conf.getRules().add(pluginRule);
+		final PluginMessage pluginMessage = new PluginMessage("AVOID_USING_STRUCTNEW");
+		pluginMessage.setSeverity("INFO");
+		cfBugs = new CFLint(conf, new StructNewChecker());
+	}
+
+	@Test
+	public void testStructNewInScript() throws ParseException, IOException {
+		final String scriptSrc = "<cfscript>\r\n"
+			+ "var a = 23;\r\n"
+			+ "var b = structNew();\r\n"
+			+ "</cfscript>";
+			
+		cfBugs.process(scriptSrc, "test");
+		final List<BugInfo> result = cfBugs.getBugs().getBugList().values().iterator().next();
+		assertEquals(1, result.size());
+		assertEquals("AVOID_USING_STRUCTNEW", result.get(0).getMessageCode());
+		assertEquals(3, result.get(0).getLine());
+	}
+
+	@Test
+	public void testStructNewInTag() throws ParseException, IOException {
+		final String tagSrc = "<cfset a = 23>\r\n"
+			+ "<cfset b = structNew()>";
+			
+		cfBugs.process(tagSrc, "test");
+		final List<BugInfo> result = cfBugs.getBugs().getBugList().values().iterator().next();
+		assertEquals(1, result.size());
+		assertEquals("AVOID_USING_STRUCTNEW", result.get(0).getMessageCode());
+		assertEquals(2, result.get(0).getLine());
+	}
+
+}

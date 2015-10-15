0b23ab37a0b555ce6376c1284e2a00478ee1d6c3
Justin Mclean <justin@classsoftware.com>
2015-10-15 15:25:53 +1100

    Split warning into two one for global (i.e. constants across many files) or local (i.e. constants in the same file).

--- src/main/java/com/cflint/plugins/core/LiteralChecker.java
+++ src/main/java/com/cflint/plugins/core/LiteralChecker.java
@@ -7,8 +7,10 @@ import ro.fortsoft.pf4j.Extension;
 import cfml.parsing.cfscript.CFExpression;
 import cfml.parsing.cfscript.CFFunctionExpression;
 import cfml.parsing.cfscript.CFLiteral;
+import cfml.parsing.cfscript.script.CFCompDeclStatement;
 import cfml.parsing.cfscript.script.CFExpressionStatement;
 import cfml.parsing.cfscript.script.CFScriptStatement;
+
 
 import com.cflint.BugInfo;
 import com.cflint.BugList;
@@ -19,10 +21,13 @@ import com.cflint.plugins.Context;
 public class LiteralChecker extends CFLintScannerAdapter {
 	final String severity = "WARNING";
 	final protected int REPEAT_THRESHOLD = 3;
+	final protected int WARNING_THRESHOLD = 5;
+	
+	protected int threshold = REPEAT_THRESHOLD;
+	protected int warningThreshold = WARNING_THRESHOLD;
 
-	protected int threshold = 3;
-	protected Map<String, Integer> literals = new HashMap<String, Integer>();
-	protected Map<String, Boolean> done = new HashMap<String, Boolean>();
+	protected Map<String, Integer> globalLiterals = new HashMap<String, Integer>();
+	protected Map<String, Integer> functionListerals = new HashMap<String, Integer>();
 
 	// May want to consider resetting literal map on new components but this way it
 	// detects duplicated literals across files which is useful
@@ -30,35 +35,61 @@ public class LiteralChecker extends CFLintScannerAdapter {
 	@Override	
 	public void expression(final CFExpression expression, final Context context, final BugList bugs) {
 		String repeatThreshold = getParameter("maximum");
-		int threshold = REPEAT_THRESHOLD;
+		String maxWarnings = getParameter("maxWarnings");
+		String warningScope = getParameter("warningScope");
 
 		if (repeatThreshold != null) {
 			threshold = Integer.parseInt(repeatThreshold);
 		}
 
+		if (maxWarnings != null) {
+			warningThreshold = Integer.parseInt(maxWarnings);
+		}
+
 		if (expression instanceof CFLiteral) {
 			CFLiteral literal = (CFLiteral) expression;
 			String name = literal.Decompile(0).replace("'","");
-			int count = 1;
 
 			if (isCommon(name)) {
 				return;
 			}
 
-			if (literals.get(name) == null) {
-				literals.put(name, count);
-				done.put(name, false);
+			int lineNo = literal.getLine() + context.startLine() - 1;
+
+			if (warningScope == null || warningScope.equals("global")) {
+				literalCount(name, lineNo, globalLiterals, true, context, bugs);
+			}
+			else if (warningScope.equals("local")) {
+				literalCount(name, lineNo, functionListerals, false, context, bugs);
+			}
+		}
+	}
+
+	@Override
+	public void expression(final CFScriptStatement expression, final Context context, final BugList bugs) {
+		if (expression instanceof CFCompDeclStatement) {
+			functionListerals.clear();
+		}
+	}
+
+	protected void literalCount(final String name, final int lineNo, Map<String, Integer> literals, boolean global, final Context context, final BugList bugs) {
+		int count = 1;
+
+		if (literals.get(name) == null) {
+			literals.put(name, count);
+		}
+		else {
+			count = literals.get(name);
+			count++;
+			literals.put(name, count);
+		}
+
+		if (count > threshold && (warningThreshold == -1 || (count - threshold) <= warningThreshold)) {
+			if (global) {
+				magicGlobalValue(name, lineNo, context, bugs);
 			}
 			else {
-				count = literals.get(name);
-				count++;
-				literals.put(name, count);
-			}
-
-			if (count > threshold && !done.get(name)) {
-				int lineNo = literal.getLine() + context.startLine() - 1;
-				magicValue(name, lineNo, context, bugs);
-				done.put(name, true);
+				magicLocalValue(name, lineNo, context, bugs);
 			}
 		}
 	}
@@ -67,10 +98,17 @@ public class LiteralChecker extends CFLintScannerAdapter {
 		return name.equals("1") || name.equals("0") || name.equals("") || name.equals("true") || name.equals("false");
 	}
 
-	public void magicValue(final String name, final int lineNo, final Context context, final BugList bugs) {
-		bugs.add(new BugInfo.BugInfoBuilder().setLine(lineNo).setMessageCode("LITERAL_VALUE_USED_TOO_OFTEN")
+	public void magicLocalValue(final String name, final int lineNo, final Context context, final BugList bugs) {
+		bugs.add(new BugInfo.BugInfoBuilder().setLine(lineNo).setMessageCode("LOCAL_LITERAL_VALUE_USED_TOO_OFTEN")
 			.setSeverity(severity).setFilename(context.getFilename())
-			.setMessage("Literal " + name + " occurs several times. Consider giving it a name and not hard coding values.")
+			.setMessage("Literal " + name + " occurs several times in the same file. Consider giving it a name and not hard coding values.")
+			.build());
+	}
+
+	public void magicGlobalValue(final String name, final int lineNo, final Context context, final BugList bugs) {
+		bugs.add(new BugInfo.BugInfoBuilder().setLine(lineNo).setMessageCode("GLOBAL_LITERAL_VALUE_USED_TOO_OFTEN")
+			.setSeverity(severity).setFilename(context.getFilename())
+			.setMessage("Literal " + name + " occurs several times in one or more files. Consider giving it a name and not hard coding values.")
 			.build());
 	}
 }

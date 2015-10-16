be6ddbe9c09da2d26645d09a231f54589863853b
Justin Mclean <justin@classsoftware.com>
2015-10-16 18:05:38 +1100

    fix line number calculation where it pure script with no tags.

--- src/main/java/com/cflint/plugins/Context.java
+++ src/main/java/com/cflint/plugins/Context.java
@@ -144,7 +144,10 @@ public class Context {
 	}
 
 	public int startLine() {
-		return element.getSource().getRow(element.getBegin());
+		if(element != null && element.getSource() !=null)
+			return element.getSource().getRow(element.getBegin());
+		else 
+			return 1; // not zero
 	}
 
 	protected String componentFromFile(String filename) {

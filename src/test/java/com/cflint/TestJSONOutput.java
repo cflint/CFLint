b35aaa1bd8950ce6c2920866044a6029d8a981e9
Justin Mclean <justin@classsoftware.com>
2016-02-21 13:47:58 +1100

    Added tests for stats

--- src/test/java/com/cflint/TestJSONOutput.java
+++ src/test/java/com/cflint/TestJSONOutput.java
@@ -24,6 +24,7 @@ public class TestJSONOutput {
 	private JSONOutput outputer;
 	private BugList bugList;
 	private Writer writer;
+
 	@Before
 	public void setUp(){
 		outputer = new JSONOutput();
@@ -31,13 +32,23 @@ public class TestJSONOutput {
 		bugList = new BugList(null);
 		writer = new StringWriter();
 	}
+
 	@Test
 	public void testOutput() throws IOException {
 		BugInfo bugInfo = new BugInfo.BugInfoBuilder().setFunction("testf").setMessageCode("PARSE_ERROR").setFilename("c:\\temp\\test.cfc").build();
 		bugList.add(bugInfo);
-		outputer.output(bugList, writer);
+		outputer.output(bugList, writer, false);
 		String expectedText = "[{\"severity\":\"\",\"id\":\"PARSE_ERROR\",\"message\":\"PARSE_ERROR\",\"category\":\"CFLINT\",\"abbrev\":\"PE\",\"location\":{\"file\":\"c:\\\\temp\\\\test.cfc\",\"fileName\":\"test.cfc\",\"function\":\"testf\",\"column\":\"0\",\"line\":\"0\",\"message\":\"\",\"variable\":\"\",\"expression\":\"\"}}]";
 //		assertEquals(JSONValue.parse(expectedText),JSONValue.parse(writer.toString()));
+		assertEquals(expectedText,writer.toString());
+	}
+
+	@Test
+	public void testStats() throws IOException {
+		BugInfo bugInfo = new BugInfo.BugInfoBuilder().setFunction("testf").setMessageCode("PARSE_ERROR").setFilename("c:\\temp\\test.cfc").build();
+		bugList.add(bugInfo);
+		outputer.output(bugList, writer, true);
+		String expectedText = "[{\"severity\":\"\",\"id\":\"PARSE_ERROR\",\"message\":\"PARSE_ERROR\",\"category\":\"CFLINT\",\"abbrev\":\"PE\",\"location\":{\"file\":\"c:\\\\temp\\\\test.cfc\",\"fileName\":\"test.cfc\",\"function\":\"testf\",\"column\":\"0\",\"line\":\"0\",\"message\":\"\",\"variable\":\"\",\"expression\":\"\"}},{\"code\":\"PARSE_ERROR\",\"count\":\"1\"}]";
 		assertEquals(expectedText,writer.toString());
 	}
 	
--- src/test/java/com/cflint/TestXMLOutput.java
+++ src/test/java/com/cflint/TestXMLOutput.java
@@ -24,20 +24,37 @@ public class TestXMLOutput {
 	private XMLOutput outputer;
 	private BugList bugList;
 	private Writer writer;
+
 	@Before
 	public void setUp(){
 		outputer = new XMLOutput();
 		bugList = new BugList(null);
 		writer = new StringWriter();
 	}
+
 	@Test
 	public void testOutput() throws IOException {
 		BugInfo bugInfo = new BugInfo.BugInfoBuilder().setFunction("testf").setMessageCode("PARSE_ERROR").setFilename("c:\\temp\\test.cfc").build();
 		bugList.add(bugInfo);
-		outputer.output(bugList, writer);
+		outputer.output(bugList, writer, false);
 		String expectedText = "<issues version=\"\">\n" +
 "<issue severity=\"\" id=\"PARSE_ERROR\" message=\"PARSE_ERROR\" category=\"CFLint\" abbrev=\"PE\"><location file=\"c:\\temp\\test.cfc\" fileName=\"test.cfc\" function=\"testf\" column=\"0\" line=\"0\" message=\"\" variable=\"\"><Expression><![CDATA[]]></Expression></location>\n" +
 "</issue>\n" +
+"</issues>";
+		assertEquals(expectedText.replace("\n", "").replace("\r", ""),writer.toString().replace("\n", "").replace("\r", ""));
+	}
+
+	@Test
+	public void testStats() throws IOException {
+		BugInfo bugInfo = new BugInfo.BugInfoBuilder().setFunction("testf").setMessageCode("PARSE_ERROR").setFilename("c:\\temp\\test.cfc").build();
+		bugList.add(bugInfo);
+		outputer.output(bugList, writer, true);
+		String expectedText = "<issues version=\"\">\n" +
+"<issue severity=\"\" id=\"PARSE_ERROR\" message=\"PARSE_ERROR\" category=\"CFLint\" abbrev=\"PE\"><location file=\"c:\\temp\\test.cfc\" fileName=\"test.cfc\" function=\"testf\" column=\"0\" line=\"0\" message=\"\" variable=\"\"><Expression><![CDATA[]]></Expression></location>\n" +
+"</issue>\n" +
+"<counts>\n" +
+"<count code=\"PARSE_ERROR\" count=\"1\" />\n" +
+"</counts>" +
 "</issues>";
 		assertEquals(expectedText.replace("\n", "").replace("\r", ""),writer.toString().replace("\n", "").replace("\r", ""));
 	}

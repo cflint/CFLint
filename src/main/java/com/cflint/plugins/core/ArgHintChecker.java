8550adf130e7b622f73b150090c2929a139c542f
Ryan <ryaneberly@gmail.com>
2016-05-11 21:15:02 -0400

    Argument hint checker uses the config.

--- src/main/java/com/cflint/plugins/core/ArgHintChecker.java
+++ src/main/java/com/cflint/plugins/core/ArgHintChecker.java
@@ -16,11 +16,7 @@ public class ArgHintChecker extends CFLintScannerAdapter {
 			final String name = element.getAttributeValue("name");
 			final String hint = element.getAttributeValue("hint");
 			if (hint == null || hint.length() == 0) {
-				int begLine = element.getSource().getRow(element.getBegin());
-				bugs.add(new BugInfo.BugInfoBuilder().setLine(begLine).setMessageCode("ARG_HINT_MISSING")
-					.setSeverity(severity).setFilename(context.getFilename()).setFunction(context.getFunctionName())
-					.setMessage("Argument " + name + " is missing a hint.")
-					.build());
+				context.addMessage("ARG_HINT_MISSING", name);
 			}
 		}
 	}

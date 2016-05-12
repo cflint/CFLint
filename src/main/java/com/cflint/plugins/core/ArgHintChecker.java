e56db9ca565e6e79c8719f2e6dccbacba7c8f571
Ryan <ryaneberly@gmail.com>
2016-05-11 21:17:34 -0400

    cleanup

--- src/main/java/com/cflint/plugins/core/ArgHintChecker.java
+++ src/main/java/com/cflint/plugins/core/ArgHintChecker.java
@@ -1,6 +1,5 @@
 package com.cflint.plugins.core;
 
-import com.cflint.BugInfo;
 import com.cflint.BugList;
 import com.cflint.plugins.CFLintScannerAdapter;
 import com.cflint.plugins.Context;
@@ -8,7 +7,6 @@ import com.cflint.plugins.Context;
 import net.htmlparser.jericho.Element;
 
 public class ArgHintChecker extends CFLintScannerAdapter {
-	final String severity = "INFO";
 
 	@Override
 	public void element(final Element element, final Context context, final BugList bugs) {

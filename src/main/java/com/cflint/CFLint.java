2d316e555bedffd1005ff5d681ccc74850041d43
stecto01 <tomasz.stec@stepstone.com>
2016-07-25 09:13:08 +0200

    Workaround context leaking.
    In some situations CFMLParser mixes content of analyzed files. That's why we need to create separate instance of CFMLParser for each file.

--- src/main/java/com/cflint/CFLint.java
+++ src/main/java/com/cflint/CFLint.java
@@ -77,7 +77,7 @@ public class CFLint implements IErrorReporter {
 	private static final String FILE_ERROR = "FILE_ERROR";
 	private static final String PARSE_ERROR = "PARSE_ERROR";
 	public static final String PLUGIN_ERROR = "PLUGIN_ERROR:";
-	final CFMLParser cfmlParser = new CFMLParser();
+	CFMLParser cfmlParser = new CFMLParser();
 	
 	
 	StackHandler handler = new StackHandler();
@@ -298,7 +298,7 @@ public class CFLint implements IErrorReporter {
 		}
 	}
 
-	
+
 	private void process(final Element elem, final String space, Context context)
 			throws ParseException, IOException {
 		context.setInComponent(inComponent);
@@ -946,6 +946,8 @@ public class CFLint implements IErrorReporter {
 	}
 
 	protected void fireStartedProcessing(final String srcidentifier) {
+		cfmlParser = new CFMLParser();
+		cfmlParser.setErrorReporter(this);
 		currentFile = srcidentifier;
 		for (final CFLintStructureListener structurePlugin : getStructureListeners(extensions)) {
 			try{

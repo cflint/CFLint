b9069044384418eaf0257b007335401f3a476594
Ryan <ryaneberly@gmail.com>
2017-01-20 22:45:32 -0500

    AVOID_USING_INLINE_JS

--- src/main/java/com/cflint/plugins/core/ScriptTagChecker.java
+++ src/main/java/com/cflint/plugins/core/ScriptTagChecker.java
@@ -1,6 +1,5 @@
 package com.cflint.plugins.core;
 
-import com.cflint.BugInfo;
 import com.cflint.BugList;
 import com.cflint.plugins.CFLintScannerAdapter;
 import com.cflint.plugins.Context;
@@ -10,6 +9,7 @@ import cfml.parsing.cfscript.script.CFScriptStatement;
 import net.htmlparser.jericho.Element;
 import ro.fortsoft.pf4j.Extension;
 
+// Deprecate?
 @Extension
 public class ScriptTagChecker extends CFLintScannerAdapter {
 	final String message = "Don't use inline <script> tags";
@@ -31,11 +31,7 @@ public class ScriptTagChecker extends CFLintScannerAdapter {
 		if (element.getName().equals("script")) {
 			final String src = element.getStartTag().toString();
 			if (!src.matches(".*src=.*")) {
-				// int endLine = element.getSource().getRow(element.getEnd());
-				final int begLine = element.getSource().getRow(element.getBegin());
-				// int total = endLine - begLine;
-				bugs.add(new BugInfo.BugInfoBuilder().setLine(begLine).setMessageCode("AVOID_USING_INLINE_JS")
-						.setSeverity(severity).setFilename(context.getFilename()).setMessage(message).build());
+				context.addMessage("AVOID_USING_INLINE_JS", null);
 			}
 		}
 	}

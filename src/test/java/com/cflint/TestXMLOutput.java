4bf4f36f77f01e218406b8832cf6087bd644f22a
Kai Koenig <kai@ventego-creative.co.nz>
2017-06-25 16:43:31 +1200

    #278 Injecting stats object into XML/HTML Output, exposing totalfiles and totalsize to CFLint output XML (in <counts>)

--- src/test/java/com/cflint/TestXMLOutput.java
+++ src/test/java/com/cflint/TestXMLOutput.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.math.BigInteger;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -26,7 +27,7 @@ public class TestXMLOutput {
 	public void testOutput() throws IOException {
 		BugInfo bugInfo = new BugInfo.BugInfoBuilder().setFunction("testf").setMessageCode("PARSE_ERROR").setFilename("c:\\temp\\test.cfc").build();
 		bugList.add(bugInfo);
-		outputer.output(bugList, writer, false);
+		outputer.output(bugList, writer, false, null);
 		String expectedText = "<issues version=\"" + Version.getVersion() + "\">\n" +
 "<issue severity=\"\" id=\"PARSE_ERROR\" message=\"PARSE_ERROR\" category=\"CFLint\" abbrev=\"PE\"><location file=\"c:\\temp\\test.cfc\" fileName=\"test.cfc\" function=\"testf\" column=\"1\" line=\"1\" message=\"\" variable=\"\"><Expression><![CDATA[]]></Expression></location>\n" +
 "</issue>\n" +
@@ -39,11 +40,14 @@ public class TestXMLOutput {
 	public void testStats() throws IOException {
 		BugInfo bugInfo = new BugInfo.BugInfoBuilder().setFunction("testf").setMessageCode("PARSE_ERROR").setFilename("c:\\temp\\test.cfc").build();
 		bugList.add(bugInfo);
-		outputer.output(bugList, writer, true);
+		CFLintStats stats = new CFLintStats();
+		stats.fileCount = 1;
+		stats.totalSize = new BigInteger("545454");
+		outputer.output(bugList, writer, true, stats);
 		String expectedText = "<issues version=\"" + Version.getVersion() + "\">\n" +
 "<issue severity=\"\" id=\"PARSE_ERROR\" message=\"PARSE_ERROR\" category=\"CFLint\" abbrev=\"PE\"><location file=\"c:\\temp\\test.cfc\" fileName=\"test.cfc\" function=\"testf\" column=\"1\" line=\"1\" message=\"\" variable=\"\"><Expression><![CDATA[]]></Expression></location>\n" +
 "</issue>\n" +
-"<counts>\n" +
+"<counts totalfiles=\"1\" totalsize=\"545454\">\n" +
 "<count code=\"PARSE_ERROR\" count=\"1\" />\n" +
 "</counts>" +
 "</issues>";

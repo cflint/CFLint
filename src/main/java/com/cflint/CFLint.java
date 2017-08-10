fcb3f7535d87d289e75e98ba7f373e9edf9bc0ac
Justin Mclean <justin@classsoftware.com>
2017-08-10 16:18:30 +1000

    remove unneeded imports

--- src/main/java/com/cflint/CFLint.java
+++ src/main/java/com/cflint/CFLint.java
@@ -8,8 +8,6 @@ import java.io.StringReader;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;

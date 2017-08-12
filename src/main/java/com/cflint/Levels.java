f0a852a3efe92487e9090c475d6da11b2b1cd8b7
Justin Mclean <justin@classsoftware.com>
2017-08-12 10:55:58 +1000

    add enum of levels

---
+++ src/main/java/com/cflint/Levels.java
@@ -0,0 +1,33 @@
+package com.cflint;
+
+public enum Levels {
+    FATAL("FATAL"),
+    CRITICAL("CRITICAL"),
+    ERROR("ERROR"),
+    WARNING("WARNING"),
+    CAUTION("CAUTION"),
+    INFO("INFO"),
+    COSMETIC("COSMETIC"),
+    UNKNOWN("UNKNOWN");
+
+    private final String severity;
+
+    private Levels(String name) {
+        severity = name;
+    }
+
+    @Override
+    public String toString() {
+       return this.severity;
+    }
+
+    public static Levels fromString(String severity) {
+        for (Levels level : Levels.values()) {
+            if (level.toString().equals(severity)) {
+                return level;
+            }
+        }
+
+        return UNKNOWN;
+    }
+}

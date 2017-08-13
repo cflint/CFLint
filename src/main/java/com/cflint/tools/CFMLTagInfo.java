24e0938db6823a944373d3ef314377bb258c10fb
ryaneberly <ryaneberly@gmail.com>
2017-08-13 13:54:00 -0400

    cleanup

--- src/main/java/com/cflint/tools/CFMLTagInfo.java
+++ src/main/java/com/cflint/tools/CFMLTagInfo.java
@@ -1,7 +1,5 @@
 package com.cflint.tools;
 
-import java.util.Map;
-
 import cfml.dictionary.Parameter;
 import cfml.dictionary.Return;
 import cfml.dictionary.SyntaxDictionary;
@@ -9,50 +7,53 @@ import cfml.dictionary.Tag;
 import net.htmlparser.jericho.Element;
 
 public class CFMLTagInfo {
-    
+
     final SyntaxDictionary dictionary;
-    public CFMLTagInfo(SyntaxDictionary dictionary) {
-        this.dictionary=dictionary;
+
+    public CFMLTagInfo(final SyntaxDictionary dictionary) {
+        this.dictionary = dictionary;
     }
 
     /**
      * 
      * @param elem
      * @param attributeName
-     * @return true when the tag/attribute combination represents a variable assignment.
+     * @return true when the tag/attribute combination represents a variable
+     *         assignment.
      */
     public boolean isAssignmentAttribute(final Element elem, final String attributeName) {
-        if(elem != null){
-            return isAssignmentAttribute(elem.getName(),attributeName);
+        if (elem != null) {
+            return isAssignmentAttribute(elem.getName(), attributeName);
         }
         return false;
     }
-    
+
     /**
      * 
      * @param elementName
      * @param attributeName
-     * @return true when the tag/attribute combination represents a variable assignment.
+     * @return true when the tag/attribute combination represents a variable
+     *         assignment.
      */
     public boolean isAssignmentAttribute(final String elementName, final String attributeName) {
-        if(elementName!=null && attributeName != null){
-            //Hardcoded exceptions to the dictionary
-            switch(elementName.toLowerCase()){
-                case "cfprocparam":
-                    return attributeName.equalsIgnoreCase("variable");
-                    
+        if ((elementName != null) && (attributeName != null)) {
+            // Hardcoded exceptions to the dictionary
+            switch (elementName.toLowerCase()) {
+            case "cfprocparam":
+                return attributeName.equalsIgnoreCase("variable");
+
             }
             final Tag tag = dictionary.getTag(elementName.toLowerCase());
-            if(tag != null){
-                for(final Object retObj : tag.getReturns()){
-                   final Return ret = (Return) retObj;
-                   if(attributeName.toLowerCase().equals(ret.getParameterName())){
-                       return true;
-                   }
+            if (tag != null) {
+                for (final Object retObj : tag.getReturns()) {
+                    final Return ret = (Return) retObj;
+                    if (attributeName.toLowerCase().equals(ret.getParameterName())) {
+                        return true;
+                    }
                 }
-                for(final Object paramObj : tag.getParameters()){
+                for (final Object paramObj : tag.getParameters()) {
                     final Parameter param = (Parameter) paramObj;
-                    if(attributeName.toLowerCase().equals(param.getName())){
+                    if (attributeName.toLowerCase().equals(param.getName())) {
                         return "variablename".equalsIgnoreCase(param.getReturnVarType());
                     }
                 }
@@ -60,40 +61,43 @@ public class CFMLTagInfo {
         }
         return false;
     }
-    
+
     /**
      * 
      * @param elem
      * @param attributeName
-     * @return true when the tag/attribute combination represents a variable refernce.
+     * @return true when the tag/attribute combination represents a variable
+     *         refernce.
      */
     public boolean isExpressionAttribute(final Element elem, final String attributeName) {
-        if(elem != null){
-            return isExpressionAttribute(elem.getName(),attributeName);
+        if (elem != null) {
+            return isExpressionAttribute(elem.getName(), attributeName);
         }
         return false;
     }
+
     /**
      * 
      * @param elementName
      * @param attributeName
-     * @return true when the tag/attribute combination represents a variable refernce.
+     * @return true when the tag/attribute combination represents a variable
+     *         refernce.
      */
     public boolean isExpressionAttribute(final String elementName, final String attributeName) {
-        if(isAssignmentAttribute(elementName,attributeName)){
+        if (isAssignmentAttribute(elementName, attributeName)) {
             return true;
         }
-        if(elementName!=null && attributeName != null){
+        if ((elementName != null) && (attributeName != null)) {
             final Tag tag = dictionary.getTag(elementName.toLowerCase());
-            if(tag != null){
-                for(final Object paramObj : tag.getParameters()){
+            if (tag != null) {
+                for (final Object paramObj : tag.getParameters()) {
                     final Parameter param = (Parameter) paramObj;
-                    if(attributeName.toLowerCase().equals(param.getName())){
+                    if (attributeName.toLowerCase().equals(param.getName())) {
                         return "query".equalsIgnoreCase(param.getType())
-                                ||"variableName".equalsIgnoreCase(param.getType())
-                                ||"array".equalsIgnoreCase(param.getType())
-                                ||"struct".equalsIgnoreCase(param.getType())
-                                ||"any".equalsIgnoreCase(param.getType());
+                                || "variableName".equalsIgnoreCase(param.getType())
+                                || "array".equalsIgnoreCase(param.getType())
+                                || "struct".equalsIgnoreCase(param.getType())
+                                || "any".equalsIgnoreCase(param.getType());
                     }
                 }
             }

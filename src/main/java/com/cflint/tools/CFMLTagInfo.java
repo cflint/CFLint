710b4c2656bf91a3d18b7960b57b1a87076c6a25
Justin Mclean <justin@classsoftware.com>
2017-08-19 10:42:21 +1000

    equalsIgnoreCase preferred

--- src/main/java/com/cflint/tools/CFMLTagInfo.java
+++ src/main/java/com/cflint/tools/CFMLTagInfo.java
@@ -38,7 +38,7 @@ public class CFMLTagInfo {
     public boolean isAssignmentAttribute(final String elementName, final String attributeName) {
         if ((elementName != null) && (attributeName != null)) {
             // Hardcoded exceptions to the dictionary
-            if (elementName.toLowerCase().equals("cfprocparam")) {
+            if (elementName.equalsIgnoreCase("cfprocparam")) {
                 return attributeName.equalsIgnoreCase("variable");
             }
             final Tag tag = dictionary.getTag(elementName.toLowerCase());

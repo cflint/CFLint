7a1230060e469e4150902b5efa15ad98b6192317
Justin Mclean <justin@classsoftware.com>
2017-08-30 11:03:04 +1000

    add private constructor

--- src/main/java/com/cflint/Version.java
+++ src/main/java/com/cflint/Version.java
@@ -4,6 +4,10 @@ import com.cflint.main.CFLintMain;
 
 public class Version {
 
+    private Version() {
+        throw new IllegalStateException("Version utility class");
+    }
+
     public static String getVersion() {
 
         String version = CFLintMain.class.getPackage().getImplementationVersion();

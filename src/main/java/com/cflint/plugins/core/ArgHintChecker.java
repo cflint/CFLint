4128635995272866e8b807d9c706406cd3b4a49c
Justin Mclean <justin@classsoftware.com>
2017-09-03 02:09:14 +1000

    refactor and add some java docs

--- src/main/java/com/cflint/plugins/core/ArgHintChecker.java
+++ src/main/java/com/cflint/plugins/core/ArgHintChecker.java
@@ -19,8 +19,14 @@ import cfml.parsing.cfscript.script.CFFunctionParameter;
 import cfml.parsing.cfscript.script.CFScriptStatement;
 import net.htmlparser.jericho.Element;
 
+/**
+ * Check if a functions argument is missing a hint.
+ */
 public class ArgHintChecker extends CFLintScannerAdapter {
 
+    /**
+     * Parse a CF argument tag to see if the argument hint is missing.
+     */
     @Override
     public void element(final Element element, final Context context, final BugList bugs) {
         if (element.getName().equals(CF.CFARGUMENT)) {
@@ -32,6 +38,9 @@ public class ArgHintChecker extends CFLintScannerAdapter {
         }
     }
 
+    /**
+     * Parse a CFScript function declaration to see if any of the arguments hints are missing.
+     */
     @Override
     public void expression(final CFScriptStatement expression, final Context context, final BugList bugs) {
         if (expression instanceof CFFuncDeclStatement) {
@@ -39,32 +48,10 @@ public class ArgHintChecker extends CFLintScannerAdapter {
             final String multiLineText = PrecedingCommentReader.getMultiLine(context, expression.getToken());
             final String mlText = multiLineText == null ? null
                     : multiLineText.replaceFirst("^/\\*", "").replaceAll("\\*/$", "").trim();
-
-            // Read the function comments to get the javadoc style annotations
+            
             final Map<String, String> annotations = new HashMap<>();
             if (mlText != null && !mlText.isEmpty()) {
-                final Pattern pattern = Pattern.compile("^.*\\s*@(\\w+)\\s+(.*+)$");
-                BufferedReader reader = new BufferedReader(new StringReader(mlText));
-                try {
-                    String line = reader.readLine();
-                    while (line != null) {
-                        final Matcher matcher = pattern.matcher(line.trim());
-                        if (matcher.matches()) {
-                            annotations.put(matcher.group(1).trim().toLowerCase(), matcher.group(2).trim());
-                        }
-                        line = reader.readLine();
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } finally {
-                    try {
-                        if (reader != null) {
-                            reader.close();
-                        }
-                    } catch (IOException e) {
-                    }
-                }
-
+                readCommentAnnotations(mlText, annotations);
             }
 
             for (final CFFunctionParameter expr : funcDeclStatement.getFormals()) {
@@ -72,7 +59,36 @@ public class ArgHintChecker extends CFLintScannerAdapter {
                     context.addMessage("ARG_HINT_MISSING_SCRIPT", expr.getName());
                 }
             }
+        }
+    }
 
+    /**
+     * Parse a function comment to see if there are any argument annotations.
+     *
+     * @param mlText multi line text to search for argument hints in comments.
+     * @param annotations function argumnet hints.
+     */
+    private void readCommentAnnotations(final String mlText, final Map<String, String> annotations) {
+        final Pattern pattern = Pattern.compile("^.*\\s*@(\\w+)\\s+(.*+)$");
+        BufferedReader reader = new BufferedReader(new StringReader(mlText));
+        try {
+            String line = reader.readLine();
+            while (line != null) {
+                final Matcher matcher = pattern.matcher(line.trim());
+                if (matcher.matches()) {
+                    annotations.put(matcher.group(1).trim().toLowerCase(), matcher.group(2).trim());
+                }
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+            }
         }
     }
 }

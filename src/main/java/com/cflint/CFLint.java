6e14a9bd2c4838c722f2179229fbf7dd2a239d02
Ryan <ryaneberly@gmail.com>
2020-03-05 20:42:10 -0500

    #670 fixed

--- src/main/java/com/cflint/CFLint.java
+++ src/main/java/com/cflint/CFLint.java
@@ -1322,7 +1322,7 @@ public class CFLint implements IErrorReporter {
         } else if (AVOID_EMPTY_FILES.equals(msgcode)) {
             ruleInfo = new PluginInfoRule();
             final PluginMessage msgInfo = new PluginMessage(AVOID_EMPTY_FILES);
-            msgInfo.setMessageText("CF file is empty: ${file}");
+            msgInfo.setMessageText("CF file is empty: ${filename}");
             msgInfo.setSeverity(Levels.WARNING);
             ruleInfo.getMessages().add(msgInfo);
         } else if (PARSE_ERROR.equals(msgcode)) {
